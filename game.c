ca96f7c6f9542bf53ee777cb6d3a2ffe6f1bc151
leeyoungeun <yongeuni22@gmail.com>
2026-05-07 18:00:04 +0900

    game system

--- game.c
+++ game.c
@@ -3,12 +3,16 @@
 // 명령어를 받아서 실제 동작 수행
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <pthread.h>
 
 #define WIDTH 10
 #define HEIGHT 10
+
+pthread_mutex_t lock;
 
 // map 초기화
 char map[HEIGHT][WIDTH];
@@ -42,12 +46,50 @@ void draw_map() {
     fflush(stdout);
 }
 
+void* gravity(void* arg) {
+    while (1) {
+        sleep(1);
+
+        pthread_mutex_lock(&lock);
+
+        // 게임 종료 조건
+        if (map[y][x] == 'X'){
+            printf("GAME OVER\n");
+            exit(0);
+        }
+        
+        // 아래가 있으면 내려감
+        if ((y < HEIGHT - 1) && (map[y+1][x] != 'X'))
+            y++;
+        // 바닥 또는 블록 충돌
+        else {
+            // 현재 블록 고정 
+            map[y][x] = 'X';
+
+            // 새 블록 생성 
+            x = WIDTH / 2;
+            y = 0;
+        }
+        draw_map();
+    
+        pthread_mutex_unlock(&lock);
+    }
+    printf("gravity\n");
+    fflush(stdout);
+}
+
 
 int main() {
     char cmd[100];
     
     init_map();
     draw_map();
+
+    pthread_t t1;
+
+    pthread_mutex_init(&lock, NULL);
+
+    pthread_create(&t1, NULL, gravity, NULL);
 
     while (1) {
 
@@ -71,22 +113,38 @@ int main() {
 
         // left 왼쪽 이동
         else if (strcmp(cmd, "left") == 0) {
+
+            // gravity thread와 main thread와 둘다 x, y, map, draw_map()에 접근 => mutex 보호
+            pthread_mutex_lock(&lock);
+
             if ((x > 0) && (map[y][x-1] != 'X'))
                 x --;
             draw_map();
             // printf("[GAME] Move Left\n");
+
+            pthread_mutex_unlock(&lock);
         }
 
         // right 오른쪽 이동 
         else if (strcmp(cmd, "right") == 0) {
+
+            // mutex 보호
+            pthread_mutex_lock(&lock);
+            
             if ((x < WIDTH - 1) && (map[y][x + 1] != 'X'))
                 x ++;
             draw_map();
             // printf("[GAME] Move Right\n");
+
+            pthread_mutex_unlock(&lock);
         }
 
         // down 밑으로 이동
         else if (strcmp(cmd, "down") == 0) {
+
+            // mutex 보호
+            pthread_mutex_lock(&lock);
+
             if ((y < HEIGHT - 1) && (map[y + 1][x] != 'X'))
                 y ++;
             else {
@@ -99,6 +157,8 @@ int main() {
             }
             draw_map();
             // printf("[GAME] Move Down\n");
+
+            pthread_mutex_unlock(&lock);
         }
 
         // rotate 회전
--- shell.c
+++ shell.c
@@ -2,22 +2,50 @@
 // 사용자 -> shell -> 명령 전달 
 // 다른 프로그램으로 보내는 역할
 
+// pthread 버전
+// shell 역할: 키보드 입력만 담당
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/select.h>
+#include <pthread.h>
+#include <termios.h>
+
+int to_game[2];   // shell -> game
+int from_game[2]; //  game -> shell
 
 void print_prompt() {
     printf("> ");
     fflush(stdout);
 }
 
-int main() {
-    char input[100];
+void* read_game_output(void* arg) {
+    char buffer[1000000];
 
-    int to_game[2];   // shell -> game
-    int from_game[2]; //  game -> shell
+    while (1){
+        int n = read(from_game[0], buffer, sizeof(buffer)-1);
+
+        if (n>0){
+            buffer[n] = 0;
+            printf("%s", buffer);
+            fflush(stdout);
+        }
+    }
+    return NULL;
+}
+
+int main() {
+    // 입력 관련 ------------
+    struct termios oldt, newt;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    // ---------------------
+
+    char input[100];
 
     // fd[0]: read, fd[1]: write
     pipe(to_game); 
@@ -46,37 +74,50 @@ int main() {
         close(to_game[0]);    // 읽기 필요 없음
         close(from_game[1]);  // 쓰기 필요 없음
         
-        // 처음에 map 나오도록 설정
-        char buffer[1000];
-        int n = read(from_game[0], buffer, sizeof(buffer)-1);
-        buffer[n] = 0;
-        printf("%s", buffer);
+        pthread_t output_thread;
+
+        pthread_create(&output_thread, NULL, read_game_output, NULL);
         
-        // print_prompt();
         while (1) {
             
-            // 입력 받기
-            fgets(input, sizeof(input), stdin);
             
+            // 문자 하나 입력 받기 (실시간 입력)
+            char ch;
+            read(STDIN_FILENO, &ch, 1);
+
+            // 입력 변환
+            if (ch == 'a')
+                write(to_game[1], "left\n", 5);
+            else if (ch == 'd')
+                write(to_game[1], "right\n", 6);
+            else if (ch == 's')
+                write(to_game[1], "down\n", 5);
+            else if (ch == 'q') {
+                write(to_game[1], "exit\n", 5);
+                break;
+            }
+
+            // 단어 입력 받기
+            // fgets(input, sizeof(input), stdin);
+
             // 개행 제거
-            input[strcspn(input, "\n")] = 0;
+            // input[strcspn(input, "\n")] = 0;
 
             // game으로 input 전달
-            write(to_game[1], input, strlen(input));
-            write(to_game[1], "\n", 1);
+            // write(to_game[1], input, strlen(input));
+            // write(to_game[1], "\n", 1);
             
             // exit 처리
-            if (strcmp(input, "exit") == 0)
-                break;
+            // if (strcmp(input, "exit") == 0)
+            //     break;
 
-            // game 출력 받아오기
-            char buffer[100000];
-            int n = read(from_game[0], buffer, sizeof(buffer)-1);
-            buffer[n] = 0;
+            // // game 출력 받아오기
+            // char buffer[100000];
+            // int n = read(from_game[0], buffer, sizeof(buffer)-1);
+            // buffer[n] = 0;
+            // printf("%s", buffer);
 
-            printf("%s", buffer);
-
-            // print_prompt();
+            // pthread_join(output_thread, NULL);
         }
     }
 
